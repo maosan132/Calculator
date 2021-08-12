e710cf86f6c2bf5cccf824a7502646d9277896e4
maosan132 <maosan132@gmail.com>
2021-08-12 18:36:37 -0500

    Add handleClick prop to button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = (props) => (
-  <>
+const Button = (props) => {
+  const { name, handleClick } = props;
+  return (
     <button
-    type="button"
-    onClick={}
-    >{name}</button>
-  </>
-);
+      type="button"
+      onClick={() => handleClick(name)}
+      value={name}
+    >
+      {name}
+    </button>
+  );
+};
 
 Button.propTypes = {
   name: PropTypes.string,
+  handleClick: PropTypes.func.isRequired,
 };
 
 Button.defaultProps = {
   name: '',
+  handleClick: undefined,
 };
 
 export default Button;
