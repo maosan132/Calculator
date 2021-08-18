547fbfc63e6be95e24ed4588b84062220255701e
maosan132 <maosan132@gmail.com>
2021-08-18 17:33:30 -0500

    Add state using useState

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 import '../App.css';
 import calculate from '../logic/calculate';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+const App = () => {
+
+  const [total, setTotal] = useState(null);
+  const [next, setNext] = useState(null);
+  const [operation, setOperation] = useState(null);
 
   handleClick(buttonName) {
     const newState = calculate(this.state, buttonName);
