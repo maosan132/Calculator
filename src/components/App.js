61760de4cde845a8b121bd149368f91ad4ca0ea2
maosan132 <maosan132@gmail.com>
2021-08-18 21:06:27 -0500

    Finish refactoring of App comp

--- src/components/App.js
+++ src/components/App.js
@@ -5,42 +5,33 @@ import '../App.css';
 import calculate from '../logic/calculate';
 
 const App = () => {
-
   const [total, setTotal] = useState(null);
   const [next, setNext] = useState(null);
   const [operation, setOperation] = useState(null);
 
-  const incrementCount = () => {
-    setCount(count + 1);
-  };
-
-  const handleClick = buttonName => {
-    const data = {total, next, operation};
+  const handleClick = (buttonName) => {
+    const data = { total, next, operation };
     const result = calculate(data, buttonName);
-
     setTotal(result.total);
     setNext(result.next);
-    setOperation(result.operation)
+    setOperation(result.operation);
+  };
+
+  let result;
+  if (operation === null) {
+    result = total;
+  } else if (operation === '+/-') {
+    result = (next === null || next === '0') ? total : next;
+  } else {
+    result = next === null ? operation : next;
   }
 
-  render() {
-    const { total, next, operation } = this.state;
-    let result;
-    if (operation === null) {
-      result = total;
-    } else if (operation === '+/-') {
-      result = (next === null || next === '0') ? total : next;
-    } else {
-      result = next === null ? operation : next;
-    }
-
-    return (
-      <div className="App">
-        <Display className="display" result={result} />
-        <ButtonPanel clickHandler={handleClick} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <Display className="display" result={result} />
+      <ButtonPanel clickHandler={handleClick} />
+    </div>
+  );
+};
 
 export default App;
