27619a929cd56e319c823d83a50365dcedd825b5
maosan132 <maosan132@gmail.com>
2021-08-24 17:51:59 -0500

    Add routes for pages home and quotes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 
 const Calculator = () => (
-  <App />
+  <React.StrictMode>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/App" component={App} />
+        <Route path="/Quote" component={Quote} />
+      </Switch>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 ReactDOM.render(
