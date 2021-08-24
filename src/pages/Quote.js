909797351e0828c3cb54b49db8244a027de8b4a2
maosan132 <maosan132@gmail.com>
2021-08-24 18:00:43 -0500

    Implement Quotes component

---
+++ src/pages/Quote.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Quote = () => (
+  <div>
+    {/* eslint-disable-next-line */}
+    <h1 className="quoting">"Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding."<br />
+      <span>- William Paul Thurston</span>
+    </h1>
+  </div>
+);
+
+export default Quote;
