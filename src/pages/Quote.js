46cf6e2db49d8cc83e38aeb7fba4c6f863ec53b1
maosan132 <maosan132@gmail.com>
2021-08-24 19:29:41 -0500

    Implement navbar comp

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Quote = () => (
   <div>
     {/* eslint-disable-next-line */}
-    <h1 className="quoting">"Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding."<br />
+    <blockquote>"Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding."<br />
       <span>- William Paul Thurston</span>
-    </h1>
+    </blockquote>
   </div>
 );
 
