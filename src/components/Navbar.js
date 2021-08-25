f3410219c6af1ea81104e46ba48671f9b6b57e86
maosan132 <maosan132@gmail.com>
2021-08-25 02:07:17 -0500

    Fix navbar, add some styling, finish routes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => (
-  <navbar>
+  <div id="navbar">
     <h1>Math Magicians</h1>
     <div className="nav">
       <Link to="/">Home </Link>
       <Link to="/App">Calculator </Link>
       <Link to="/Quote">Quote </Link>
     </div>
-  </navbar>
+  </div>
 );
 
 export default NavBar;
