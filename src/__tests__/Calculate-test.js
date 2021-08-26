a08631ceb88d740d72dd0bc61ee71256ecee0a8c
maosan132 <maosan132@gmail.com>
2021-08-26 13:39:54 -0500

    Add imports to calculate-test.js

---
+++ src/__tests__/Calculate-test.js
@@ -0,0 +1,5 @@
+import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import Calculate from '../logic/Calculate';
+import App from '../App';
